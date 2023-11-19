fe3837c39185a24a8af43b42e64c4ffe6d40ed12
Dima Markelov <di-marko@hotmail.com>
2023-11-19 19:19:48 +0100

    removed api from config file

--- src/components/DrinksList/DrinksList.jsx
+++ src/components/DrinksList/DrinksList.jsx
@@ -4,9 +4,10 @@ import "./DrinksList.css"
 
 const DrinksList = ({config}) => {
     const [drinks, setDrinks] = useState([]);
+    const url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic"
 
     useEffect(() => {
-        fetch(config.api.drinksEndpoint)
+        fetch(url)
             .then(response => response.json())
             .then(data => {
                 setDrinks(data.drinks);
@@ -14,7 +15,7 @@ const DrinksList = ({config}) => {
             .catch(error => {
                 console.error('Error fetching data: ', error);
             });
-    }, [config.api.drinksEndpoint]);
+    }, [url]);
 
     return (
         <div className="drinks-container" style={{ 
